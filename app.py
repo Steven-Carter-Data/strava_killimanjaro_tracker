2e3e34234655c8de471d4541b9b88323f84921e4
Steven-Carter-Data <44236062+Steven-Carter-Data@users.noreply.github.com>
2024-06-18 14:06:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -47,9 +47,9 @@ workout_levels = {
     }
 }
 
-# # Add flag to the top of the title
-# flag_url = "https://github.com/Steven-Carter-Data/strava_killimanjaro_tracker/blob/main/tanzania_flag.png?raw=true"
-# st.image(flag_url, use_column_width=False, width=200)
+# Add flag to the top of the title
+flag_url = "https://github.com/Steven-Carter-Data/strava_killimanjaro_tracker/blob/main/tanzania_flag.png?raw=true"
+st.image(flag_url, use_column_width=False, width=200)
 
 # Include title in the app
 st.markdown("<div class='title-font'>Throne of Africa Strava Bourbon Chaser Competition</div>", unsafe_allow_html=True)
