e7b5bfde417849eebbd7bd01b99fa391287a50ff
Steven-Carter-Data <44236062+Steven-Carter-Data@users.noreply.github.com>
2024-06-18 12:15:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -184,7 +184,7 @@ if data is not None:
                 mode="gauge+number",
                 value=row['Zone 2 and Above Hours'],
                 title={'text': f"{row['Participant']}'s Zone 2 and Above Progress (Week {selected_week})"},
-                number={'suffix': ''},
+                number={'font': {'size': 1}},
                 gauge={
                     'axis': {'range': [None, workout_levels[row['Chosen Level']]['zone2_and_above']]},
                     'bar': {'color': "#1EB53A"},
