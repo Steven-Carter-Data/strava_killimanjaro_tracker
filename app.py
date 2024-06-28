4c2547fda0c6b370c05ead73ac3ed03897807608
Steven Carter <sdcarter22@gmail.com>
2024-06-28 10:22:38 -0400

    <new viz>

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import numpy as np
 import statsmodels
+import altair
 
 # Set app layout parameters
 st.set_page_config(layout="wide")
